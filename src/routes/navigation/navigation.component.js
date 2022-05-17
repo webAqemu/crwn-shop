c2c23d4e95a34229054507d41beb5ba0ef6e722d
Michael Notych <notych2812@gmail.com>
2022-05-17 22:18:47 +0300

    Added sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/routes/navigation/navigation.component.js
@@ -0,0 +1,27 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assests/crown.svg';
+import './navigation.styles.sass';
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='navigation__logo' to='/'>
+          <CrwnLogo />
+        </Link>
+        <div className='navigation__list'>
+          <Link className='navigation__link' to='/shop'>
+            Shop
+          </Link>
+          <Link className='navigation__link' to='/sign-in'>
+            Sign in
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,58 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDvF6WRNcfO2SMH7yYpmLtVfWzYOyC77VY',
+  authDomain: 'crwn-clothing-db-3ad05.firebaseapp.com',
+  projectId: 'crwn-clothing-db-3ad05',
+  storageBucket: 'crwn-clothing-db-3ad05.appspot.com',
+  messagingSenderId: '575993787484',
+  appId: '1:575993787484:web:2ca08ba31def6e0c4b6299',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
